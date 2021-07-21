277d84cde5da00eafd6913a730cb9e20e4a691fa
sander <sander.c.vonk@gmail.com>
2021-07-20 20:03:48 -0700

    add sound effect

--- script.js
+++ script.js
@@ -69,6 +69,41 @@ function setLinks() {
 
 }
 
+var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    65: 'a',
+    66: 'b'
+};
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+
+var konamiCodePosition = 0;
+
+document.addEventListener('keydown', function (e) {
+    var key = allowedKeys[e.keyCode];
+    var requiredKey = konamiCode[konamiCodePosition];
+    if (key == requiredKey) {
+        konamiCodePosition++;
+        if (konamiCodePosition == konamiCode.length) {
+            activateCheats();
+            konamiCodePosition = 0;
+        }
+    } else {
+        konamiCodePosition = 0;
+    }
+});
+
+function activateCheats() {
+    document.body.style.backgroundImage = "url('images/cheatBackground.png')";
+
+    var audio = new Audio('audio/coin.mp3');
+    audio.play();
+
+    alert("totally not an easter egg 🙃");
+}
 //for alt menu
 function myFunction(x) {
     x.classList.toggle("change");
