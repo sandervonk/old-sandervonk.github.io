cd45e3fd60a3a2a2f541e4d09c76f56537239e92
Sander <sander.c.vonk@gmail.com>
2021-08-02 22:18:39 -0700

    more of the same, small style changes though

--- about.js
+++ about.js
@@ -1,19 +1,20 @@
 window.addEventListener("load", function () {
+    document.getElementsByClassName("about-nav-sections")[0].id = "rotate"
     sections = document.getElementsByClassName("about-nav-sections")[0].children
-    document.getElementById("js-section-rotate1")
     sections[0].addEventListener("click", function () {
-        document.getElementsByClassName("about-nav-sections")[0].className = "about-nav-sections rotate1"
+        document.getElementsByClassName("about-nav-sections")[0].className = "about-nav-sections rotate5"
     })
     sections[1].addEventListener("click", function () {
-        document.getElementsByClassName("about-nav-sections")[0].className = "about-nav-sections rotate2"
+        document.getElementsByClassName("about-nav-sections")[0].className = "about-nav-sections rotate4"
     })
     sections[2].addEventListener("click", function () {
         document.getElementsByClassName("about-nav-sections")[0].className = "about-nav-sections rotate3"
     })
     sections[3].addEventListener("click", function () {
-        document.getElementsByClassName("about-nav-sections")[0].className = "about-nav-sections rotate4"
+        document.getElementsByClassName("about-nav-sections")[0].className = "about-nav-sections rotate2"
     })
     sections[4].addEventListener("click", function () {
-        document.getElementsByClassName("about-nav-sections")[0].className = "about-nav-sections rotate5"
+        document.getElementsByClassName("about-nav-sections")[0].className = "about-nav-sections rotate1"
     })
+
 })
