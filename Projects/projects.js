2ea0c9159af82e8aa9188fd7269c2bb578105829
Sander Vonk <sander.c.vonk@gmail.com>
2021-11-08 17:05:41 -0800

    Add Github Projects
    - Add auto-creating Github Repo Section
    - Display projects by most recent
    - Automatically add new projects + links

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,15 +1,19 @@
 function dumpResponse() {
   // `this` will refer to the `XMLHTTPRequest` object that executes this function
   let projects = JSON.parse(this.responseText),
-    html = `<div id="github-projects">`;
+    html = "";
   for (let project of projects) {
-    html += `<div class="github-project"><a href="${
-      "https://github.com/" + project["full_name"]
-    }">${project.name}</div>`;
+    html =
+      `<a class="github-project rounded-full bg-teal" "href="${
+        "https://github.com/" + project["full_name"]
+      }">${project.name}</a>` + html;
   }
+  html =
+    `<div id="github-projects"><a href="https://github.com/sandervonk" class="github-label rounded-full bg-purple"><img src="/img/github.svg" id="github-label-img" />Github Projects</a>` +
+    html;
   html += "</div>";
+  document.getElementById("github-section-parent").innerHTML += html;
   console.log(html);
-  document.getElementById("github-section").innerHtml = html;
 }
 var request = new XMLHttpRequest();
 // Set the event handler
