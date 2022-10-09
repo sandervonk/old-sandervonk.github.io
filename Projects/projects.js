bdce73f4b6a77d10303b20e8afa27e8cf3286294
Sander Vonk <sander.c.vonk@gmail.com>
2022-10-09 15:15:16 -0700

    fix project order

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,28 +1,18 @@
 function dumpResponse() {
   // `this` will refer to the `XMLHTTPRequest` object that executes this function
   let projects = JSON.parse(this.responseText),
-    html = "";
+    html = `<div id="github-projects"><a href="https://github.com/sandervonk" class="github-label rounded-full bg-purple"><img src="/img/github.svg" id="github-label-img" />Github Projects</a>`;
   for (let project of projects) {
-    html =
-      `<a class="github-project rounded-full bg-teal" href="${
-        "https://github.com/" + project["full_name"]
-      }">${project.name}</a>` + html;
+    html += `<a class="github-project rounded-full bg-teal" title="${"https://github.com/" + project["full_name"]}" href="${"https://github.com/" + project["full_name"]}">${project.name}</a>`;
   }
-  html =
-    `<div id="github-projects"><a href="https://github.com/sandervonk" class="github-label rounded-full bg-purple"><img src="/img/github.svg" id="github-label-img" />Github Projects</a>` +
-    html;
   html += "</div>";
-  document.getElementById("github-section-parent").innerHTML += html;
+  document.getElementById("github-section-parent").innerHTML = html;
   console.log(html);
 }
 var request = new XMLHttpRequest();
 // Set the event handler
 request.onload = dumpResponse;
 // Initialize the request
-request.open(
-  "get",
-  "https://api.github.com/users/sandervonk/repos?per-page=20",
-  true
-);
+request.open("get", "https://api.github.com/users/sandervonk/repos?per-page=100&sort=updated&order=desc", true);
 // Fire away!
 request.send();
