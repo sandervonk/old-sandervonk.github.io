ad5603a8ec8fd9f544f6d15ac49604e18ef716f8
Sander Vonk <sander.c.vonk@gmail.com>
2022-11-24 22:14:33 -0800

    improve projects page

--- Projects/projects.js
+++ Projects/projects.js
@@ -7,7 +7,6 @@ function dumpResponse() {
   }
   html += "</div>";
   document.getElementById("github-section-parent").innerHTML = html;
-  console.log(html);
 }
 var request = new XMLHttpRequest();
 // Set the event handler
@@ -16,3 +15,52 @@ request.onload = dumpResponse;
 request.open("get", "https://api.github.com/users/sandervonk/repos?per-page=100&sort=updated&order=desc", true);
 // Fire away!
 request.send();
+$.fn.isInViewport = function () {
+  let elementTop = $(this).offset().top;
+  let elementBottom = elementTop + $(this).outerHeight();
+  let viewportTop = $(window).scrollTop() + 71;
+  let viewportBottom = viewportTop + $(window).height();
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+function getContrast(hexcolor) {
+  return parseInt(hexcolor.replace("#", ""), 16) > 0xffffff / 2 ? "#000000" : "#ffffff";
+}
+function getContrastAccent(hexcolor) {
+  return parseInt(hexcolor.replace("#", ""), 16) > 0xffffff / 2 ? "#add8e680" : "#d7d7d745";
+}
+// scroll listener to update theme-color meta tag with color of active (w/ jquery)
+$(".projects-wrapper").scroll(function () {
+  // get in-view child of .projects-wrapper
+  let inView = $(
+    $(".projects-wrapper")
+      .children()
+      .filter(function () {
+        return $(this).isInViewport();
+      })[0]
+  );
+  // get color of in-view child
+  let color = inView.children("div.container").css("background-color");
+  color = `#${color
+    .match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/)
+    .slice(1)
+    .map((n) => parseInt(n, 10).toString(16).padStart(2, "0"))
+    .join("")}`;
+  if ($(".projects-wrapper").scrollTop() == 0) {
+    color = "#ffffff";
+  }
+  // update theme-color meta tag
+  $("meta[name=theme-color], meta[name='apple-mobile-web-app-status-bar']").attr("content", color);
+  //update #titleBar background color
+  $("#titleBar, .sideMenu").css("background-color", color);
+  //update #titlebar accent color to have enough contrast
+  let contrastingColor = getContrast(color);
+  if (contrastingColor == "#000000") {
+    $("#invert-title").css("filter", "invert(0)");
+  } else {
+    $("#invert-title").css("filter", "invert(1)");
+  }
+  $("body").css("--accent-transparent", getContrastAccent(color));
+  $("#titleBar, .sideMenu").css("color", contrastingColor);
+  $("#titleBar, .sideMenu").css("border-color", contrastingColor + "4d");
+});
--- script.js
+++ script.js
@@ -1,113 +1,109 @@
 function transition() {
-    highlightCurrentPage()
-    setupSelection()
-    if (window.location.href.includes("Contact.html")) {
-        setLinks()
-    }
-    document.querySelector("img[src='../img/SiteName.png']").addEventListener("click", function () {
-        window.location.href = "https://sander.vonk.one"
-    })
+  highlightCurrentPage();
+  setupSelection();
+  if (window.location.href.includes("Contact.html")) {
+    setLinks();
+  }
+  document.querySelector("img[src='../img/SiteName.png']").addEventListener("click", function () {
+    window.location.href = "https://sander.vonk.one";
+  });
 }
 function highlightCurrentPage() {
-    window.addEventListener("load", transition)
-    pageId = window.location.href.split("?")[0].split("/")[window.location.href.split("/").length - 2]
-    pageId = pageId.replace("About", "About_Me")
-    if (pageId != "" && pageId != "404") {
-        document.getElementById(pageId).style = "background-color: rgba(173, 216, 230, 0.5) !important; transition: background-color 1.5s;"
-        document.getElementById(pageId + "2").style = "background-color: rgba(173, 216, 230, 0.5) !important; transition: background-color 1.5s;"
-    } else {
-        pageId = "Contact"
-        document.getElementById(pageId).style = "background-color: rgba(173, 216, 230, 0.5) !important; transition: background-color 1.5s;"
-    }
+  window.addEventListener("load", transition);
+  pageId = window.location.href.split("?")[0].split("/")[window.location.href.split("/").length - 2];
+  pageId = pageId.replace("About", "About_Me");
+  if (pageId != "" && pageId != "404") {
+    document.getElementById(pageId).style = "background-color: var(--accent-transparent) !important; transition: background-color 0.25s;";
+    document.getElementById(pageId + "2").style = "background-color: var(--accent-transparent) !important; transition: background-color 0.25s;";
+  } else {
+    pageId = "Contact";
+    document.getElementById(pageId).style = "background-color: var(--accent-transparent) !important; transition: background-color 0.25s;";
+  }
 }
 
-
 function clearSelections() {
-    var selections = document.querySelectorAll('[original-content]');
-    selections.forEach(function (selection) {
-        selection.innerHTML = selection.getAttribute('original-content');
-    });
+  var selections = document.querySelectorAll("[original-content]");
+  selections.forEach(function (selection) {
+    selection.innerHTML = selection.getAttribute("original-content");
+  });
 }
 
 function setupSelection() {
-    document.body.addEventListener('mousedown', function () {
-        if (document.body.classList.contains('selections-enabled')) {
-            document.body.classList.remove('selections-enabled');
-            clearSelections();
-        }
-    });
+  document.body.addEventListener("mousedown", function () {
+    if (document.body.classList.contains("selections-enabled")) {
+      document.body.classList.remove("selections-enabled");
+      clearSelections();
+    }
+  });
 }
 function toGit() {
-    window.location.href = "https://github.com/sandervonk"
-    console.log("toGit called")
+  window.location.href = "https://github.com/sandervonk";
+  console.log("toGit called");
 }
 function toMail() {
-    window.location.href = "mailto:sander.c.vonk@gmail.com"
-    console.log("toMail called")
+  window.location.href = "mailto:sander.c.vonk@gmail.com";
+  console.log("toMail called");
 }
 function setLinks() {
-    console.log("settingLinks")
-    document.getElementById('git-parent').onclick = toGit
-    document.getElementById('mail-parent').onclick = toMail
+  console.log("settingLinks");
+  document.getElementById("git-parent").onclick = toGit;
+  document.getElementById("mail-parent").onclick = toMail;
 
-    const element1 = document.querySelector("#git-parent");
-    const element2 = document.querySelector("#mail-parent");
+  const element1 = document.querySelector("#git-parent");
+  const element2 = document.querySelector("#mail-parent");
 
-    element1.addEventListener("mouseover", event => {
-        element2.style = "transform: translate(-50%, -50%) rotate(-130deg) translate(180px) rotate(130deg);"
-    });
+  element1.addEventListener("mouseover", (event) => {
+    element2.style = "transform: translate(-50%, -50%) rotate(-130deg) translate(180px) rotate(130deg);";
+  });
 
-    element1.addEventListener("mouseout", event => {
-        element2.style.transform = ""
-    });
-    element2.addEventListener("mouseover", event => {
-        element1.style = "transform: translate(-50%, -50%) rotate(-179deg) translate(180px) rotate(179deg);"
-    });
+  element1.addEventListener("mouseout", (event) => {
+    element2.style.transform = "";
+  });
+  element2.addEventListener("mouseover", (event) => {
+    element1.style = "transform: translate(-50%, -50%) rotate(-179deg) translate(180px) rotate(179deg);";
+  });
 
-    element2.addEventListener("mouseout", event => {
-        element1.style.transform = ""
-    });
-
-
+  element2.addEventListener("mouseout", (event) => {
+    element1.style.transform = "";
+  });
 }
 //totally not an easter egg
 var allowedKeys = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down',
-    65: 'a',
-    66: 'b'
+  37: "left",
+  38: "up",
+  39: "right",
+  40: "down",
+  65: "a",
+  66: "b",
 };
-var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
-
+var konamiCode = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a"];
 
 var konamiCodePosition = 0;
 
-document.addEventListener('keydown', function (e) {
-    var key = allowedKeys[e.keyCode];
-    var requiredKey = konamiCode[konamiCodePosition];
-    if (key == requiredKey) {
-        konamiCodePosition++;
-        if (konamiCodePosition == konamiCode.length) {
-            activateCheats();
-            konamiCodePosition = 0;
-        }
-    } else {
-        konamiCodePosition = 0;
+document.addEventListener("keydown", function (e) {
+  var key = allowedKeys[e.keyCode];
+  var requiredKey = konamiCode[konamiCodePosition];
+  if (key == requiredKey) {
+    konamiCodePosition++;
+    if (konamiCodePosition == konamiCode.length) {
+      activateCheats();
+      konamiCodePosition = 0;
     }
+  } else {
+    konamiCodePosition = 0;
+  }
 });
 
 function activateCheats() {
-    document.body.style.backgroundImage = "url('images/cheatBackground.png')";
+  document.body.style.backgroundImage = "url('images/cheatBackground.png')";
 
-    var audio = new Audio('audio/coin.mp3');
-    audio.play();
+  var audio = new Audio("audio/coin.mp3");
+  audio.play();
 
-    alert("totally not an easter egg 🙃");
+  alert("totally not an easter egg 🙃");
 }
 //for alt menu
 function myFunction(x) {
-    x.classList.toggle("change");
+  x.classList.toggle("change");
 }
-window.addEventListener("load", transition)
+window.addEventListener("load", transition);
